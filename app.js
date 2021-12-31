20f24897dd8915939022ceb344439a6f17ddd288
nilslv <nils@levity.ai>
2021-12-31 10:23:29 +0100

    deleted js file

--- app.js
+++
@@ -1,6 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-let param = urlParams.get("text");
-param = param != null ? param : "Try it out yourself!";
-
-console.log(param);
-document.getElementById("input").innerHTML = param;
